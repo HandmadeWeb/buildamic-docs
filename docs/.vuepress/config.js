73bd9f2896cdc6d972f43abccef72decee0a3623
Yohan <john@inertiainmotion.com.au>
2022-04-13 10:45:32 +1000

    Add vuepress config

---
+++ docs/.vuepress/config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  // site config
+  lang: 'en-US',
+  title: 'Hello, VuePress!',
+  description: 'This is my first VuePress site',
+
+  // theme and its config
+  theme: '@vuepress/theme-default',
+  themeConfig: {
+    logo: 'https://www.handmadeweb.com.au/wp-content/uploads/2015/04/Handmade-Web-and-Design-Logo-Black-200px.png',
+  },
+}
